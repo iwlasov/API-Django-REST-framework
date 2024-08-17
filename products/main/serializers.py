f91c2ff418b0cc9a00ff0da5d26401a176824c41
iwlasov <iwlasov@gmail.com>
2024-08-17 14:40:59 +0300

    initial commit

---
+++ products/main/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from main.models import Product, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    # реализуйте все поля
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+class ProductListSerializer(serializers.Serializer):
+    # реализуйте поля title и price
+    title = serializers.CharField()
+    price = serializers.DecimalField(10, 2)
+
+class ProductDetailsSerializer(serializers.ModelSerializer):
+    # реализуйте поля title, description, price и reviews (список отзывов к товару)
+    rewiews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price', 'rewiews']
